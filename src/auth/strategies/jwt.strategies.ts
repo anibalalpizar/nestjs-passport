33f6596d8403c966b0590ede2fbb4ed352971e23
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-22 01:57:18 -0600

    add JWT strategy for user authentication using Passport

---
+++ src/auth/strategies/jwt.strategies.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategies extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secret',
+    });
+  }
+
+  validate(payload: any) {
+    console.log('inside validate', payload);
+    return payload;
+  }
+}
