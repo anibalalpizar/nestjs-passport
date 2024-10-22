34a5e7ac984ed1121f19d3d2b4d4c53e26491869
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-22 01:57:42 -0600

    add getProfile endpoint with JwtGuard for user profile retrieval

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   Post,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import type { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { LocalGuard } from './guards/local.guard';
+import type { Request } from 'express';
+import { JwtGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -19,5 +23,11 @@ export class AuthController {
     const user = this.authService.validateUser(authPayload);
     if (!user) throw new HttpException('Invalid username or password', 401);
     return user;
+  }
+
+  @Get('profile')
+  @UseGuards(JwtGuard)
+  getProfile(@Req() req: Request) {
+    return req.user;
   }
 }
